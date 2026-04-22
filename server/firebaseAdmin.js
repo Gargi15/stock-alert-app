66ebdcb663cce104e0c2062a731adf05d027e17a
Gargi Biswas <gabiswas@Gargis-MacBook-Air.local>
2026-04-22 10:40:56 +0530

    untracking service accoutns file

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -6,17 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "serviceAccountKey.json"), "utf-8")
-);
-
-
-
-
-// // Load your service account JSON
-// const serviceAccount = JSON.parse(
-//   fs.readFileSync("./serviceAccountKey.json", "utf-8")
-// );
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
